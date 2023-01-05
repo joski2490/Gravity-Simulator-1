a681016de5b4ea420864b39f72f9d74a2942e562
joski2490 <68449917+joski2490@users.noreply.github.com>
2023-01-05 09:41:47 -0600

    Update spacetime.js

--- js/gravity/modules/spacetime.js
+++ js/gravity/modules/spacetime.js
@@ -58,7 +58,7 @@ define([
 		// Takes in object, returns radius from object mass and density
 		function getObjectRadius(object){
 			var radius = Math.cbrt(
-				object.mass*object.density*massMultiplier
+				(object.mass*object.density*massMultiplier) / (8/3*Math.PI)
 			);
 			
 			return radius;
